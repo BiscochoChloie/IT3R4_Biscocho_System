a32b6c17f9d1542ec806f2cfc4f291a81cb21e52
BiscochoChloie <chloiebiscocho123@gmail.com>
2020-12-19 19:25:07 +0800

    Implementing Security Layer

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,8 @@ $router->get('/users/{id}', 'UserController@read');
 $router->put('/users/{id}', 'UserController@update');
 
 $router->delete('/users/{id}', 'UserController@destroy');
+
+// userjob routes
+$router->get('/usersjob','UserJobController@index');
+
+$router->get('/usersjob/{id}','UserJobController@show'); 
